2335ec2ccd539faae3ed7cd90fe5df972cdbda07
dealer426 <burnssamuel01@gmail.com>
2020-01-14 13:55:33 -0500

    First Code Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //Entry Point
+        //Method
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+           //Enter your Email Address 
+            string userName = "emailme@gmail.com";
+
+            Console.WriteLine("Here is my name " + userName);
+
+            //Set Number Here
+            int myNumber = 426;
+
+            Console.WriteLine("Here is my number: {0}", myNumber);
+
+            Console.WriteLine("Here is my name " + userName + " and number {0} ", myNumber);
+
+            Console.ReadLine();
+
+        }
+    }
+}
